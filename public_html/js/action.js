79499cf33bc82dce260be1f24183130a80003ef5
WanJu Lin <t2roioaui@gmail.com>
2022-07-21 16:56:26 +0800

    auto focus

--- public_html/js/action.js
+++ public_html/js/action.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
-  
+
+    $('.date-block').dblclick(function(e){
+        $('#info-panel').addClass('open').css({
+            top:e.pageY+'px',
+            left:e.pageX+'px',
+        }).find('.title [contenteditable]').focus();
+       
+    });
+
+    $('#info-panel')
+    .on('click','button', function(e){
+        if($(this).is('.create')){
+
+        }
+        if($(this).is('.update')){
+
+        }
+        if($(this).is('.cancel')){
+            $('#info-panel').removeClass('open');
+            
+
+        }
+        if($(this).is('.delete')){
+
+        }
+
+    })
+    .on('click','.close',function(e){
+        $('button.cancel').click();
+
+    });
+
   });
